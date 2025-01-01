2dfbfb0880ca9a579033c240058bdaf8420ba9b9
Harsh Pandey <pandey73harsh@gmail.com>
2025-01-01 14:48:51 +0530

    Time: 0 ms (100%), Space: 8.6 MB (61.74%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int s2n0(char a){
-        return a=='0'?1:0;
-    }
-    int s2n1(char a){
-        return a=='0'?0:1;
-    }
     int maxScore(string s) {
-        int maxsum = INT_MIN;
         int n = s.length();
-        int sumi = 0;
-        for(int i = 0; i < n-1; i++){
-            sumi += s2n0(s[i]);
-            int sumj = 0;
-            for(int j = i+1; j < n; j++){
-                sumj += s2n1(s[j]);
-            }
-            maxsum = max(maxsum, sumi+sumj);
+        int totalone = 0;
+        int maxsum = INT_MIN;
+        for(int i = 0; i < n; i++){
+            totalone += s[i]=='0'?0:1;
         }
+        int zeroes = 0;
+        for(int i = 0; i < n-1; i++){
+            if(s[i]=='0'){
+                zeroes++;
+            } else {
+                totalone--;
+            }
+
+            maxsum = max(maxsum, totalone+zeroes);
+        }
+
         return maxsum;
     }
 };
