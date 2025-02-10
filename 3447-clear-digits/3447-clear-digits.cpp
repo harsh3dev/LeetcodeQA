bd65ddd560e282c5e35d51bf0e58fdce6b2458f1
Harsh Pandey <pandey73harsh@gmail.com>
2025-02-10 07:49:22 +0530

    Time: 0 ms (100%), Space: 8.5 MB (56.73%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string result;
+        for(char c:s){
+            if(isdigit(c)){
+                result.pop_back();
+            } else {
+                result.push_back(c);
+            }
+        }
+
+        return result;
+    }
+};
