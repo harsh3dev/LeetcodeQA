7180415d9615a310f427215a0a3ee531ac8cdb65
Harsh Pandey <pandey73harsh@gmail.com>
2025-02-15 19:28:34 +0530

    Time: 771 ms (8.06%), Space: 538.9 MB (5.54%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
@@ -1,35 +1,40 @@
 class Solution {
 private:
-    bool getSum(string str, int num, int i, int sum){
-        if (i >= str.length()){
+    bool getSum(string str, int num, int i, int sum, vector<vector<int>>& t) {
+        if (i >= str.length()) {
             return sum == num;
         }
 
-        if (sum > num){
+        if (sum > num) {
             return false;
         }
 
+        if(t[i][sum] != -1){
+            return t[i][sum];
+        }
+
         bool possible = false;
-        for(int j = i; j < str.length(); j++){
+        for (int j = i; j < str.length(); j++) {
             string sub = str.substr(i, j - i + 1);
             int val = stoi(sub);
-            possible = possible || getSum(str, num, j+1, sum+val);
-            if (possible == true){
+            possible = possible || getSum(str, num, j + 1, sum + val, t);
+            if (possible == true) {
                 return true;
             }
         }
 
-        return possible;
+        return t[i][sum] = possible;
     }
 
 public:
     int punishmentNumber(int n) {
 
         int totalSum = 0;
-        
-        for(int i = 1; i <= n; i++){
+
+        for (int i = 1; i <= n; i++) {
             int multiplication = i * i;
-            if (getSum(to_string(multiplication), i, 0, 0)){
+            vector<vector<int>> t(to_string(multiplication).length(), vector<int>(i+1, -1));
+            if (getSum(to_string(multiplication), i, 0, 0, t)) {
                 totalSum += multiplication;
             }
         }
@@ -37,3 +42,17 @@ public:
         return totalSum;
     }
 };
+
+// first take all the nums from 1 to n
+// for each num check if its valid or not
+// for a num there will be multiple combinations of substrings
+// we have to check for all the combinations
+// base case will be if a combination the sum > num
+// then we return false else we return true
+// to calculate the sum, we get the initial sum
+// then we add the next number which comes from i to j
+// if that is greater we return false
+// else we continue to add more
+// then we increment i and j
+// if after adding we get more we return false we get same then we return true
+// if we get less we return false
