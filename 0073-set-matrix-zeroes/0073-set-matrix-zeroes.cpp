df2a7693b4ce8c0bbdaaa25e0786e3749fad8a0d
Harsh Pandey <pandey73harsh@gmail.com>
2025-04-04 18:09:05 +0530

    Time: 0 ms (100%), Space: 18.6 MB (79.49%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int rowindex = matrix.size();
-        int colindex = matrix[0].size();
-        int col0 = 1;
-        
-        for (int i = 0; i < rowindex; i++) {
-            if (matrix[i][0] == 0) col0 = 0;
-            for (int j = 1; j < colindex; j++) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int firstcol = -1;
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] == 0)
+                firstcol = 0;
+            for (int j = 1; j < m; j++) {
                 if (matrix[i][j] == 0) {
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
@@ -15,13 +15,14 @@ public:
             }
         }
 
-        for (int i = rowindex - 1; i >= 0; i--) {
-            for (int j = colindex - 1; j >= 1; j--) {
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 1; j--) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
-            if (col0 == 0) matrix[i][0] = 0;
+            if (firstcol == 0)
+                matrix[i][0] = 0;
         }
     }
 };
