6cd430f13d5145f285c9460658dcf4a4be9910f6
Harsh Pandey <pandey73harsh@gmail.com>
2025-04-07 11:19:11 +0530

    Time: 0 ms (100%), Space: 8.7 MB (17.44%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    int m[46];
-    int solve(int n) {
-        // base cases
-        if (n < 0)
-            return 0;
-        if (n == 0)
-            return 1;
-        if (m[n] != -1) {
-            return m[n];
-        }
-        int step1 = solve(n - 1);
-        int step2 = solve(n - 2);
-
-        return m[n] = step1 + step2;
-    }
-
     int climbStairs(int n) {
-        memset(m, -1, sizeof(m));
-        return solve(n);
+        vector<int> m(n + 1, -1);
+        if (n <= 3)
+            return n;
+        m[0] = 0;
+        m[1] = 1;
+        m[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            m[i] = m[i - 1] + m[i - 2];
+        }
+        return m[n];
     }
 };
