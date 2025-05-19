eee6f08a499a4b9530cbb7487c90cb84e8b1e00f
Harsh Pandey <pandey73harsh@gmail.com>
2025-05-19 22:56:13 +0530

    Time: 0 ms (100%), Space: 17.7 MB (57.34%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int prev = 1;
-        int back = 1;
+       int ans = INT_MIN;
+       int pdt = 1;
+       int mul = 1;
+       
+       int n = nums.size();
 
-        int ans = INT_MIN;
-
-        int n = nums.size();
-        for(int i = 0; i < n; i++){
-            if(prev == 0){
-                prev = 1;
+       for(int i = 0; i < n; i++){
+            if(pdt == 0){
+                pdt = 1;
             }
-            if(back == 0){
-                back = 1;
+            if(mul == 0){
+                mul = 1;
             }
 
-            prev *= nums[i];
-            back *= nums[n-i-1];
+            pdt *= nums[i];
+            mul *= nums[n - i - 1];
 
-            ans = max(ans, max(prev, back));
-        }
+            ans = max(ans, max(pdt, mul));
+       }
 
-        return ans;
+       return ans;
     }
 };
+
+// we have to find the subarray which will yeild the maximum pdt
+// if there are even -ve integers then pdt will be max, else min
+// so we have to remove one -ve int if there are odd ones
+// so we will calculate the pdt from back and front and whichever is max we will update it
+// if 0 is encountered then we have to reset the pdt to 1
