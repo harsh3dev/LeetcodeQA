4bdf8e33e87d5f2f50d961ec6836606371422118
Harsh Pandey <pandey73harsh@gmail.com>
2025-06-07 23:50:13 +0530

    Time: 388 ms (16.88%), Space: 106.9 MB (18.5%) - LeetHub

--- 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     string clearStars(string s) {
-        vector<stack<int>> cnt(26);
+    priority_queue<pair<char,int>, vector<pair<char,int>>, greater<pair<char,int>>> minh;
+    unordered_map<int,bool> mpp;
         for (int i = 0; i < s.size(); i++) {
-            if (s[i] != '*') {
-                cnt[s[i] - 'a'].push(i);
+            if(s[i] == '*') {
+                mpp[-minh.top().second] = true;
+                minh.pop();
             } else {
-                for (int j = 0; j < 26; j++) {
-                    if (!cnt[j].empty()) {
-                        s[cnt[j].top()] = '*';
-                        cnt[j].pop();
-                        break;
-                    }
-                }
+                minh.push({s[i], -i});
             }
         }
-
-        string ans;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] != '*') {
-                ans.push_back(s[i]);
-            }
+        string res;
+        for(int i = 0; i < s.size(); i++) {
+            if(mpp.count(i) || s[i] == '*') continue;
+            res += s[i];
         }
-        return ans;
+        return res;
     }
 };
