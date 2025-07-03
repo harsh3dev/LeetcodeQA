8dc0fc142e1f96a11977a650f15b402d037eddb9
Harsh Pandey <pandey73harsh@gmail.com>
2025-07-03 21:45:22 +0530

    Time: 0 ms (100%), Space: 9.1 MB (70.42%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-        string w = "abbcbccdbccdcddebccdcddecddedeefbccdcddecddedeefcddedeefdeefeffgbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhibccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhicddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhideefeffgeffgfggheffgfgghfgghghhieffgfgghfgghghhifgghghhighhihiij";
-        return w[k-1];
+        string sb = "a";
+        while (sb.length() < k) {
+            int size = sb.length();
+            for (int i = 0; i < size; ++i) {
+                char nextChar = 'a' + ((sb[i] - 'a' + 1) % 26);
+                sb += nextChar;
+            }
+        }
+        return sb[k - 1];
     }
 };
