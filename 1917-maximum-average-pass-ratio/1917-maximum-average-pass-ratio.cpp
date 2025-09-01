908d0e7b09e531d7d9497875f6c4e58f1d612b19
Harsh Pandey <pandey73harsh@gmail.com>
2025-09-01 13:27:16 +0530

    Time: 271 ms (90.06%), Space: 97.9 MB (66.37%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
@@ -1,37 +1,30 @@
 class Solution {
 private:
-    double calcProfit(int pass, int total){
-        return (double)(pass+1)/(total+1) - (double)pass/total;
+    double calcProfit(int pass, int total) {
+        return (double)(pass + 1) / (total + 1) - (double)pass / total;
     }
+
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
         // push initials to the pq
         priority_queue<pair<double, int>> pq;
-        for(int i = 0; i < classes.size(); i++){
-            pq.push(
-                {
-                    calcProfit(classes[i][0], classes[i][1]),
-                    i
-                }
-            );
+        for (int i = 0; i < classes.size(); i++) {
+            pq.push({calcProfit(classes[i][0], classes[i][1]), i});
         }
 
         // update the classes with extrastudents
-        for(int i = 0; i < extraStudents; i++){
+        for (int i = 0; i < extraStudents; i++) {
             auto [gain, idx] = pq.top();
             pq.pop();
             classes[idx][0]++;
             classes[idx][1]++;
 
-            pq.push({
-                calcProfit(classes[idx][0], classes[idx][1]),
-                idx
-            });
+            pq.push({calcProfit(classes[idx][0], classes[idx][1]), idx});
         }
 
         double avgsum = 0;
-        for(int i = 0; i < classes.size(); i++){
-            avgsum += (double)classes[i][0]/classes[i][1];
+        for (int i = 0; i < classes.size(); i++) {
+            avgsum += (double)classes[i][0] / classes[i][1];
         }
 
         avgsum /= classes.size();
@@ -40,6 +33,6 @@ public:
     }
 };
 // adding an extra student will increase both the total no and the pass
-// adding more and more extra students to the same class will decrease the rate of increase eventually
-// so we have to add in that class which will give more gain
-// so we maintain a pq which will maintain the highest gain
+// adding more and more extra students to the same class will decrease the rate
+// of increase eventually so we have to add in that class which will give more
+// gain so we maintain a pq which will maintain the highest gain
