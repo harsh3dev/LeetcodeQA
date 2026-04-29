1a228184623d635d6950c8784cf0a155a6aad91f
Harsh Pandey <pandey73harsh@gmail.com>
2026-04-29 22:41:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.go
+++ 0070-climbing-stairs/0070-climbing-stairs.go
@@ -1,29 +1,15 @@
-func solve(curr int, n int, m []int) int {
-    if curr > n {
-        return 0
-    }
-    if curr == n {
-        return 1
-    }
-
-    if m[curr] != -1 {
-        return m[curr]
-    }
-
-    var two int = solve(curr + 2, n, m)
-    var one int = solve(curr + 1, n, m)
-    m[curr] = two + one
-    return m[curr]
-}
-
 func climbStairs(n int) int {
-    var curr int = 0
+    if n < 2 {
+        return n
+    }
+    s := make([]int, n+1)
 
-    m := make([]int, 46)
+    s[1] = 1
+    s[2] = 2
 
-    for i := range m {
-        m[i] = -1
+    for i := 3; i <= n; i++ {
+        s[i] = s[i-1] + s[i-2]
     }
 
-    return solve(curr, n, m)
+    return s[n]
 }
