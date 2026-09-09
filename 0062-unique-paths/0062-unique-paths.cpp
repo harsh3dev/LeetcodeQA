527fb1f1806f26d72f391008bef3cfb7fcb8ca6b
Harsh Pandey <pandey73harsh@gmail.com>
2026-09-09 10:29:34 +0530

    Time: 3 ms (9.61%), Space: 9.5 MB (5.27%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    private:
+    int solve(int m, int n, int i, int j){
+        if(i == m || j == n){
+            return 0;
+        }
+
+        if(i == m-1 && j == n-1){
+            return 1;
+        }
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+
+        return t[i][j] = solve(m, n, i, j+1) + solve(m, n, i+1, j);
+    }
+public:
+    vector<vector<int>> t;
+    int uniquePaths(int m, int n) {
+        t.resize(m, vector<int>(n));
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                t[i][j] = -1;
+            }
+        }
+
+        return solve(m, n, 0, 0);
+    }
+};
